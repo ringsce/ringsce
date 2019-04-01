afb07cf66b274c6e8c3bbcefeebad6530839e6ff
Pedro Dias Vicente <pedrolvicente@gmail.com>
2019-04-01 14:35:36 +0100

    4th Commit Beta

--- Linux/mainwindow.h
+++ Linux/mainwindow.h
@@ -1,100 +1,20 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-#include <QMap>
-#include <QPointer>
 
-class QAction;
-class QComboBox;
-class QFontComboBox;
-class QTextEdit;
-class QTextCharFormat;
-class QMenu;
-class QPrinter;
-
-
-class QTextEdit;
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    //textEdit(QWidget *parent = 0);
-
-    QTextEdit *editor;
-    //QTextEdit *textEdit;
-
-    bool load(const QString &f);
-
-    MainWindow(QWidget *parent = 0);
-
-private slots:
-    //void fileOpen();
-    bool fileSave();
-    //bool fileSaveAs();
-    void filePrint();
-    void filePrintPreview();
-    void filePrintPdf();
-
-    void textBold();
-    void textUnderline();
-    void textItalic();
-    void textFamily(const QString &f);
-    void textSize(const QString &p);
-    void textStyle(int styleIndex);
-    void textColor();
-    void textAlign(QAction *a);
-
-    void currentCharFormatChanged(const QTextCharFormat &format);
-    void cursorPositionChanged();
-
-    void clipboardDataChanged();
-    void printPreview(QPrinter *);
-
-    void fileNew();
-    void about();
-    void LunarAbout();
-    void newFile();
-    void openFile(const QString &path = QString());
-
-protected:
-    void closeEvent(QCloseEvent *e) override;
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
 
 private:
-    void setupFileMenu();
-    void setupEditor();
-    bool maybeSave();
-    void mergeFormatOnWordOrSelection(const QTextCharFormat &format);
-    void fontChanged(const QFont &f);
-    void colorChanged(const QColor &c);
-    void alignmentChanged(Qt::Alignment a);
-
-    QAction *actionSave;
-    QAction *actionTextBold;
-    QAction *actionTextUnderline;
-    QAction *actionTextItalic;
-    QAction *actionTextColor;
-    QAction *actionAlignLeft;
-    QAction *actionAlignCenter;
-    QAction *actionAlignRight;
-    QAction *actionAlignJustify;
-    QAction *actionUndo;
-    QAction *actionRedo;
-#ifndef QT_NO_CLIPBOARD
-    QAction *actionCut;
-    QAction *actionCopy;
-    QAction *actionPaste;
-#endif
-
-    QComboBox *comboStyle;
-    QFontComboBox *comboFont;
-    QComboBox *comboSize;
-
-    QToolBar *tb;
-    QString fileName;
-
+    Ui::MainWindow *ui;
 };
-
 #endif // MAINWINDOW_H
